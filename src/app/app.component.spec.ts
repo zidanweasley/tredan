ae4b1a9308024ab8299fc561d97fb461d3e2e902
zidanweasley <zidancock1@gmail.com>
2021-07-30 15:20:30 +0700

    resize

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('apzie-ng app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('tredan app is running!');
   });
 });
